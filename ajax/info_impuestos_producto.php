1c0531d0755d20241179ad5186f8ae768b1ec5f4
JeanCarlos Chavarria Hughes <jchavarr@akamai.com>
2019-08-14 21:06:09 -0600

    Jean: fix in info_impuestos_producto.php missing constants.php again
    Signed-off-by: JeanCarlos Chavarria Hughes <jchavarr@akamai.com>

--- ajax/info_impuestos_producto.php
+++ ajax/info_impuestos_producto.php
@@ -3,6 +3,7 @@
 	/* Connect To Database*/
 	require_once ("../config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
 	require_once ("../config/conexion.php");//Contiene funcion que conecta a la base de datos
+    include("../config/constants.php");
 
 	$id_producto = $_GET['id_producto'];
 	$query = mysqli_query($con, "select * from products where id_producto='".$id_producto."'");
